7631b5f60e07e2d9ad6311c5e3152cc08adeb683
Lin Lyu <lyulin.lynn@gmail.com>
2021-12-07 13:47:57 +0000

    added Ask.cs

--- Kiosk.App/Ask.cs
+++ Kiosk.App/Ask.cs
@@ -6,7 +6,34 @@ using System.Linq;
 namespace Kiosk.App;
 
 class Ask {
-    public void Run() {
-        Console.WriteLine("Ask");
+
+    public void Run(){
+        Question q1 = new Question();
+        q1.title = "what is your name?";
+
+        Question q2 = new Question();
+        q2.title = "what do you want to choose: Pissa, Lahmacun, Bitterball";
+        
+
+        List<Question> lists = new List<Question>();
+        lists.Add(q1);
+        lists.Add(q2);
+
+        ask(lists);
+
+    }
+
+
+    public List<Question> ask(List<Question> lists) {
+
+        foreach (var item in lists)
+        {
+            Console.WriteLine(item.title);
+            item.answer = Console.ReadLine()!;
+            Console.WriteLine("The one I want is: " + item.answer);
+
+        }
+       return null;
+        
     }
 }
